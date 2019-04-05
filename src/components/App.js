41706eaa4dfb675aa0b42888c9a8fa5a2bd4d7bf
Thang <nguyendinhthang192@gmail.com>
2019-04-05 15:30:30 +0700

    first Commmit

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return(
+            <div className="App">
+                <div className="title">
+                    Reminder Pro
+                </div>
+
+                <div className="form-inline">
+                    <div className="form-group">
+                        <input className="form-control" placeholder="I have to ..."></input>
+                    </div>
+                    <button type="button" className="btn btn-success">Add remider</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
